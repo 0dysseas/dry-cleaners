c820729466b4b8612fed0a915330793b5e582178
0dysseas <kaziolas.od@gmail.com>
2018-05-02 12:59:36 +0300

    Form tweaks

--- cleaners/forms.py
+++ cleaners/forms.py
@@ -5,7 +5,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Name',
                            required=True)
 
-    email_from = forms.EmailField(label='Email address', required=True,
+    email_from = forms.EmailField(label='E-mail address', required=True,
                                   error_messages={'invalid': 'Your e-mail does not have a valid format.'},
                                   )
 
